35ae507e3d2144c695f259330464e68c00faa308
nekita <chubarev.nk@phystech.edu>
2025-10-23 23:44:34 +0300

    fix Note

--- netodo/include/netodo/notes/Note.hpp
+++ netodo/include/netodo/notes/Note.hpp
@@ -10,13 +10,11 @@
 struct Note {
     int64_t id;
     std::string name;
-    std::string short_name;
     std::string description;
 
-    explicit Note(const std::string& name, const std::string& short_name = "",
-        const std::string& description = "");
-    Note(int64_t id, const std::string& name,
-        const std::string& short_name = "", const std::string& description = "");
+    explicit Note(const std::string& name = "", const std::string& description = "");
+
+    explicit Note(int64_t id, const std::string& name = "", const std::string& description = "");
 };
 
 enum class NoteStatus {
