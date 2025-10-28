06486777fd9db097a3e7bc9c0dddfe7f1ff7b47c
nekita <chubarev.nk@phystech.edu>
2025-10-28 14:41:50 +0300

    add exceptions testing, fix test_start

--- app/test_start.cpp
+++ app/test_start.cpp
@@ -2,17 +2,20 @@
 // Created by nekita on 29.08.25.
 //
 
+#include <fstream>
+#include <iostream>
+
 #include "netodo/App.hpp"
-#include "netodo/interface/ConsolUI.hpp"
 #include "netodo/db_manager/TaskDBManager.hpp"
 #include "netodo/db_manager/ToDoDBManager.hpp"
+#include "netodo/interface/ConsolUI.hpp"
 #include "netodo/notes/Note.hpp"
-#include "iostream"
 
 int main() {
+    std::ifstream fs("input_test_start.txt");
     TaskDBManager task_db("test_start.db");
     ToDoDBManager todo_db("test_start.db");
     App app(todo_db, task_db);
-    ConsolUI ui(std::cin, std::cout, app);
+    ConsolUI ui(fs, std::cout, app);
     ui.Show();
 }
