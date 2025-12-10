1fb10f40a5ee8b17cfc80fa8fec34d0fa77eed06
nekita <chubarev.nk@phystech.edu>
2025-12-10 16:50:16 +0300

    add main_app start

---
+++ app/main_app.cpp
@@ -0,0 +1,22 @@
+//
+// Created by nekita on 19.11.25.
+//
+
+#include <iostream>
+
+#include "netodo/App.hpp"
+#include "netodo/db_manager/TaskDBManager.hpp"
+#include "netodo/db_manager/ToDoDBManager.hpp"
+#include "netodo/interface/ConsolUI.hpp"
+
+int main() {
+    std::cout << "NeToDo start!\n";
+    std::cout << "Name of NeToDo base: ";
+    std::string base_name;
+    std::cin >> base_name;
+    TaskDBManager task_db(base_name);
+    ToDoDBManager todo_db(base_name);
+    App app(todo_db, task_db);
+    ConsolUI ui(std::cin, std::cout, app);
+    ui.Show();
+}
